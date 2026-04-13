380f7db5258d9167af89937e1d869f1d00807a80
katrinahay13 <katrinahay13@gmail.com>
2026-04-13 15:02:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-const CACHE_NAME = 'six-court-mixed-v1';
-const URLS_TO_CACHE = ['./', './index.html', './icon.svg', './manifest.webmanifest'];
-
+const CACHE_NAME = 'six-court-mixed-v3';
+const URLS_TO_CACHE = ['./','./index.html','./manifest.webmanifest','./icon-512.png','./apple-touch-icon.png','./icon.svg'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
